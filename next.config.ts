5887f3d8110e2085711a82f75e8a36cfc5c5917f
AJay Kumar Sinha <ajayksinha1089@gmail.com>
2025-12-10 09:57:38 +0530

    worked finally

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-
+  experimental: {
+  appDir: true,
+  allowDynamic: true,  // allow dynamic params during export
+  serverActions: false,
+  dynamicParams: true,   // <-- allow dynamic [id] routes without static params
+},
   // IMPORTANT: Disable Turbopack inside CI to avoid WorkerError
   webpack: (config) => {
     return config;
   },
+  skipMiddlewareUrlNormalize: true,
+  skipTrailingSlashRedirect: true,
+
 
   // or if you want turbopack but with empty config:
   // turbopack: {},
