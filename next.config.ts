a69fed23601a40e3b719cd389bb19babf6a1801b
AJay Kumar Sinha <ajayksinha1089@gmail.com>
2026-01-01 11:36:51 +0530

    final build

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,51 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  //output: 'export',          //dev branch update required for static export
-  trailingSlash: true,       // recommended for Azure
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   //output: 'export',          //dev branch update required for static export
+//   trailingSlash: true,       // recommended for Azure
+//   images: {
+//     unoptimized: true,
+//   },
+// //   experimental: {
+// //   appDir: true,
+// //   allowDynamic: true,  // allow dynamic params during export
+// //   serverActions: false,
+// //   dynamicParams: true,   // <-- allow dynamic [id] routes without static params
+// // },
+//   // IMPORTANT: Disable Turbopack inside CI to avoid WorkerError
+//   webpack: (config) => {
+//     return config;
+//   },
+//   skipMiddlewareUrlNormalize: true,
+//   skipTrailingSlashRedirect: true,
+
+
+//   // or if you want turbopack but with empty config:
+//   // turbopack: {},
+// };
+
+// module.exports = nextConfig;
+
+
+
+
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  output: 'standalone', // REQUIRED for Azure App Service
+
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
-//   experimental: {
-//   appDir: true,
-//   allowDynamic: true,  // allow dynamic params during export
-//   serverActions: false,
-//   dynamicParams: true,   // <-- allow dynamic [id] routes without static params
-// },
-  // IMPORTANT: Disable Turbopack inside CI to avoid WorkerError
-  webpack: (config) => {
-    return config;
-  },
-  skipMiddlewareUrlNormalize: true,
-  skipTrailingSlashRedirect: true,
+}
 
+export default nextConfig
 
-  // or if you want turbopack but with empty config:
-  // turbopack: {},
-};
-
-module.exports = nextConfig;
 
 
 
